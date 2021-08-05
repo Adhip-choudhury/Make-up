dcd8a79349513daa618d5b2965b64f469de91f04
Adhip-choudhury <79558085+Adhip-choudhury@users.noreply.github.com>
2021-08-05 20:44:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -10,11 +10,24 @@ function setup() {
   video = createCapture(VIDEO);
   video.size(300, 300);
   video.hide();
-
-  
+ 
+  poseNet= ml5.poseNet(video, modelLoaded);
+  poseNet.on('pose', gotPoses);
 }
 
+function modelLoaded(){
 
+}
+
+function gotPoses(results){
+  if(results.length > 0){
+    console.log(results);
+    noseX=results[0].pose.nose.x;
+    noseY=results[0].pose.nose.y;
+    console.log("Nose x="+noseX);
+    console.log("Nose y="+noseY);
+  }
+}
 
 function draw() {
   image(video, 0, 0, 400, 400);
